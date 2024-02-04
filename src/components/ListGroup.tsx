172bcad9bdc93487b19a13538eff7ba833e77b45
Menaka S <menaka442@gmail.com>
2024-02-04 22:14:38 +0530

    conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,10 +1,14 @@
 import { Fragment } from "react";
 
 function ListGroup() {
-  const items = ["Wattala", "Kandana", "Colombo", "Jaffna", "Negombo"];
+  let items = ["Wattala", "Kandana", "Colombo", "Jaffna", "Negombo"];
+  items = [];
+  // items = ["Wattala", "Kandana", "Colombo"];
+
   return (
     <Fragment>
       <h1>LK City List</h1>
+      {items.length === 0 && <p>No item found</p>} {/* trick: if first part is true, then jsx will display the second part*/}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
