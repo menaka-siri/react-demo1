189bdef78a8fbfb062f039952f6c21806f160483
Menaka S <menaka442@gmail.com>
2024-02-17 08:09:15 +0530

    use UseState to highlight the selected item in the list

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,18 @@
-import { Fragment } from "react";
-import { MouseEvent } from "react";
+import { Fragment, useState } from "react";
 
 function ListGroup() {
   let items = ["Wattala", "Kandana", "Colombo", "Jaffna", "Negombo"];
   // items = [];
   // items = ["Wattala", "Kandana", "Colombo"];
 
-  //Event handler
-  const handleClick = (event: MouseEvent) => console.log(event);
+  //State Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  /*
+  useState returns an array of two elements
+  eg: const arr = useState(-1);
+  arr[0]: variable (selectedIndex)
+  arr[1]: updater function
+  */
 
   return (
     <Fragment>
@@ -15,11 +20,17 @@ function ListGroup() {
       {items.length === 0 && <p>No item found</p>}{" "}
       {/* trick: if first part is true, then jsx will display the second part*/}
       <ul className="list-group">
-        {items.map((item) => (
+        {items.map((item, index) => (
           <li
-            className="list-group-item"
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
             key={item}
-            onClick={handleClick}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
           >
             {item}
           </li>
