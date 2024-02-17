3b005c79b3f0c689fee0f9cbeda62d3a1303961c
Menaka S <menaka442@gmail.com>
2024-02-17 11:42:43 +0530

    show alert on button click

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
+  const onAlertClose = () => {
+    setAlertVisibility(false);
+    console.log("Alert Closed");
+  }
+
   return (
     <div>
-      <Button color="danger" onClick={()=> console.log('Clicked')}>My Button</Button>
+      {alertVisible && <Alert onClose={onAlertClose}>My Alert</Alert>}
+      <Button
+        color="danger"
+        onClick={() => {
+          console.log("Clicked");
+          setAlertVisibility(true);
+        }}
+      >
+        Show Alert
+      </Button>
     </div>
   );
 }
