2577ca9d9ca46de49918ec80096935cde864b9b2
Menaka S <menaka442@gmail.com>
2024-02-17 10:59:50 +0530

    pass children to a component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["Wattala", "Kandana", "Colombo", "Jaffna", "Negombo"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <div>
-      <ListGroup items={items} heading="LK Cities" onSelectItem={handleSelectItem}></ListGroup>
+      <Alert>
+      Hello <span>World</span>
+      </Alert>
     </div>
   );
 }
