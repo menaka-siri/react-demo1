ad6bc301ffdb856c9d9853b92cfe7eae4c19ca05
Menaka S <menaka442@gmail.com>
2024-03-08 18:34:23 +0530

    add answers to practice exercises

--- src/App.tsx
+++ src/App.tsx
@@ -26,6 +26,47 @@ function App() {
 
   const [cartItems, setCartItems] = useState(["Product1", "Product2"]);
 
+  //Managing Component States - Exercise-12-a
+  /* const [game, setGame] = useState({
+    id: 1,
+    player: {
+      name: "John",
+    },
+  });
+
+  const handleClick =  () => {
+    //here the use of spread operator is to future proof the player object within game object
+    setGame({ ...game, player: { ...game.player, name: 'Bob'}})
+  } */
+
+  //Managing Component States - Exercise-12-b
+  /* const [pizza, setPizza] = useState({
+    name: 'Spicy Pepperoni',
+    toppings: ['Mushroom']
+  });
+
+  const handleClick = () => {
+    setPizza({...pizza, toppings: [...pizza.toppings, 'Mozzarella']});
+  } */
+
+  //Managing Component States - Exercise-12-b
+  /* const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
+  });
+
+  const handleClick = () => {
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+      ),
+    });
+  }; */
+
   return (
     <div>
       {alertVisible && <Alert onClose={onAlertClose}>My Alert</Alert>}
