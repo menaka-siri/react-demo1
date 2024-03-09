a09fdbaa1b0f3e401b001b16bb548bc9726010ee
Menaka S <menaka442@gmail.com>
2024-03-09 10:57:42 +0530

    apply form field validations

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,11 @@
 import React, { FormEvent, useRef, useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
 
+interface FormData {
+  name: string;
+  age: number;
+}
+
 const Form = () => {
   /*   const nameRef = useRef<HTMLInputElement>(null);
   const ageRef = useRef<HTMLInputElement>(null);
@@ -29,12 +34,15 @@ const Form = () => {
   };
  */
 
-  const { register, handleSubmit } = useForm();
-  console.log(register("name"));
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
 
   const onSubmit = (data: FieldValues) => {
-    console.log("inside onSubmit...")
-    console.log(data)
+    console.log("inside onSubmit...");
+    console.log(data);
   };
 
   return (
@@ -45,11 +53,17 @@ const Form = () => {
         </label>
         {/* <input ref={nameRef} id="name" type="text" className="form-control" /> */}
         <input
-          {...register("name")}
+          {...register("name", { required: true, minLength: 3 })}
           id="name"
           type="text"
           className="form-control"
         />
+        {errors.name?.type === "required" && (
+          <p className="text-danger">The name field is required.</p>
+        )}
+        {errors.name?.type === "minLength" && (
+          <p className="text-danger">The name must be atleast 3 characters.</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
